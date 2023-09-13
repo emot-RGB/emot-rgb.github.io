d5526e70626c6d5f702850f941e5d5b1a25b1e66
emot-RGB <emot-RGB@users.noreply.github.com>
2023-09-13 19:48:21 +0100

    stuff happen I regret being alive

--- adding_sheet_comments.js
+++ adding_sheet_comments.js
@@ -1,15 +1,4 @@
-//import content from 'emot-rgb.github.io/page_content.json' assert {type:'json'};
-//
-//console.log(content);
-//
-//
-//
-//let i;
-//
-//for (i in content) {
-//    console.log(i);
-//}
+import content from 'emot-rgb.github.io/page_content.json' assert {type:'json'};
 
-import get_data from 'emot-rgb.github.io/python code/env/include/sheets-test-google.py';
+console.log(content);
 
-console.log(get_data());
