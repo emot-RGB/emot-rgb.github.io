2f5a0413df90e44489e826e3f529241b479115e0
emot-RGB <112391963+emot-RGB@users.noreply.github.com>
2024-12-19 15:20:43 +0000

    testing stuff

--- README.md
+++
@@ -1,9 +0,0 @@
-# emot-rgb.github.io
-
-### ok this is the third time I'm blogging
-
-> # emot-rgb's blog
->>
->> --> (I don't know what I should put in a blog, bruh!!)
->
-> I still remeber where the name "emot-rgb" came from. I wanted to mix emotions with colors so I thought that I could use emot for emotion and rgb to represent the color part.
---
+++ procat_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure4754_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h82bec5d81a0fc1e4: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1da690c261a13f43: (a: number, b: number) => void;
+export const closure7534_externref_shim: (a: number, b: number, c: any) => void;
+export const closure7538_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hdec8ffaa703b7914: (a: number, b: number) => void;
+export const closure102500_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
